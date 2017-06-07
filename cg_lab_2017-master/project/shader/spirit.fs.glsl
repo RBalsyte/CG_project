df6e4c42e8930091bdefc7a2ca7a48ad1950d6aa
RBalsyte <ramintabal@gmail.com>
2017-06-07 23:32:52 +0200

    Camera js, multitexturing, sky color, cleanup
    Added Diana's committed code, just in case github messes the commit to
    master, since I stupidly didn't branch off first.

---
+++ cg_lab_2017-master/project/shader/spirit.fs.glsl
@@ -0,0 +1,10 @@
+/**
+ * as simple fragment shader just setting the provided color as fragment color
+ * Created by Samuel Gratzl on 08.02.2016.
+ */
+
+ precision mediump float;
+
+ void main() {
+ 	gl_FragColor = vec4(0,1,1,1);
+ }
